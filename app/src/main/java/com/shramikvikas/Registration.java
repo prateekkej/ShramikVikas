07d2993d0a850033326ee21aec689c6f43e6aa9f
Prateek <prateekkej@gmail.com>
2017-02-22 00:19:54 +0530

    Registration layout added and integrated. Main Activity Layout refreshed Registrations agan refreshed. Card list added. View pAger added tabs added new Graphics Recent list added animations done New layouts added. Use ourTextView class instead of TextVew class multiple layouts for multiple devicees more cleaner code categorized classes SQL Integration Started Lag gye BC! updated 22-02-17

--- app/src/main/java/com/shramikvikas/Registration.java
+++ app/src/main/java/com/shramikvikas/Registration.java
@@ -7,16 +7,22 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import okhttp3.FormBody;
+import okhttp3.RequestBody;
+
 public class Registration extends AppCompatActivity {
     private ImageView pass;
     private TextView pin;
+    private EditText fname,lname,phone,email,address;
     private Button signup;
     private boolean pass_status=true;
     @Override
@@ -30,10 +36,28 @@ public class Registration extends AppCompatActivity {
                 NavUtils.navigateUpFromSameTask(Registration.this);
             }
         });
+        fname=(EditText)findViewById(R.id.fname);
+        lname=(EditText)findViewById(R.id.lname);
+        email=(EditText)findViewById(R.id.emailid);
+        phone=(EditText)findViewById(R.id.phNumber);
+        address=(EditText)findViewById(R.id.address);
         pass = (ImageView)findViewById(R.id.pass);
         pin= (TextView)findViewById(R.id.numericpin);
+
+
         signup=(Button)findViewById(R.id.signup);
         signup.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/Raleway-Medium.ttf"));
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RequestBody requestBody= new FormBody.Builder().add("firstName",fname.getText().toString())
+                        .add("lastName",lname.getText().toString())
+                        .add("emailId",email.getText().toString())
+                        .add("phoneNumber",phone.getText().toString())
+                        .add("address",address.getText().toString())
+                        .add("Password",pin.getText().toString()).build();
+            }
+        });
         pass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
