0278bde4d589d92be9605ab6869f9c5e8448d9fa
sidupamanyu <siddharthupamanyu@gmail.com>
2017-03-31 00:32:47 +0530

    Registration Database done

---
+++ app/src/main/java/com/shramikvikas/Data/FirebaseDatabase.java
@@ -0,0 +1,8 @@
+package com.shramikvikas.Data;
+
+/**
+ * Created by R K Sharma on 30-03-2017.
+ */
+
+public class FirebaseDatabase {
+}
---
+++ app/src/main/java/com/shramikvikas/UserInformation.java
@@ -0,0 +1,25 @@
+package com.shramikvikas;
+
+/**
+ * Created by R K Sharma on 30-03-2017.
+ */
+
+public class UserInformation {
+
+    public String fname;
+    public String lname;
+    public String address;
+    public String phnumber;
+
+    public UserInformation(String fname, String lname, String address, String phnumber) {
+        this.fname = fname;
+        this.lname = lname;
+        this.address = address;
+        this.phnumber = phnumber;
+    }
+
+    public UserInformation(){
+
+    }
+
+}
